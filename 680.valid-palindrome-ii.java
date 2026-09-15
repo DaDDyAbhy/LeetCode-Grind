d54b628acdc7f26cb775316a028367018492054e
Abhyuday Singh <TUMHARA_GITHUB_EMAIL>
2026-09-16 02:25:30 +0530

    Add solution for 680. Valid Palindrome Ii

---
+++ 680.valid-palindrome-ii.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=680 lang=java
+ *
+ * [680] Valid Palindrome II
+ */
+
+// @lc code=start
+
+// Note--> Iss question mein hum palindromeHelper() function call ke samay i++ ya j-- calling ke time nahi use karenge because i++ means that kaam karr lo and then i=i+1 karna and similarly j-- mean ki kaam karlo phir j=j-1 karna.
+// And --i aur ++j ka matlab hota hai ki pehle i=i+1 hoga aur firr koi kaam hoga and --j means ki pehle j=j-1 hoga phir hi koi aur kaam hoga.
+
+class Solution {
+    private boolean palindromeHelper(int i, int j, String s) {
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i<j){
+            char left = s.charAt(i);
+            char right = s.charAt(j);
+            if(left != right){
+                return palindromeHelper(i+1, j, s) || palindromeHelper(i,j-1, s);
+            }else{
+                i++;
+                j--;
+            }
+        }
+    return true;
+    }
+}
+
+
+// @lc code=end
+
